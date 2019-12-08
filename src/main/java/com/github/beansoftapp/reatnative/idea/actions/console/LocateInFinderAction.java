e10926c55f8f9c1aed4dd0c0bd3b40b7798f6721
liuchangjiong <liucj@i-counting.cn>
2019-12-08 16:24:06 +0800

    2019.1 version, requires IDEA 2019.1 or later, fix issues in 2019.1

--- src/main/java/com/github/beansoftapp/reatnative/idea/actions/console/LocateInFinderAction.java
+++ src/main/java/com/github/beansoftapp/reatnative/idea/actions/console/LocateInFinderAction.java
@@ -1,15 +1,16 @@
 package com.github.beansoftapp.reatnative.idea.actions.console;
 
 import com.github.beansoftapp.reatnative.idea.actions.BaseRNConsoleAction;
-import com.github.beansoftapp.reatnative.idea.actions.RevealFileAction;
 import com.github.beansoftapp.reatnative.idea.icons.PluginIcons;
 import com.github.beansoftapp.reatnative.idea.utils.RNPathUtil;
 import com.github.beansoftapp.reatnative.idea.views.ReactNativeConsole;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.util.SystemInfo;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
+import java.lang.reflect.Method;
 
 // Show NPM project in finder/explorer
 public class LocateInFinderAction extends BaseRNConsoleAction {
@@ -21,9 +22,32 @@ public class LocateInFinderAction extends BaseRNConsoleAction {
         String npmLocation = RNPathUtil.getRNProjectPath(getProject());
 
         if (npmLocation != null) {
-            RevealFileAction.openFile(new File(npmLocation + File.separatorChar + "package.json"));
+            File file = new File(npmLocation + File.separatorChar + "package.json");
+            openFile(file);
         }
     }
+
+    // Compatible with all IDEA 2019 versions
+    public static void openFile(File file) {
+        try {
+            Class revealFileAction = Class.forName("com.intellij.ide.actions.RevealFileAction");// IDEA 2019.3+
+            Method method = revealFileAction.getMethod("openFile",  java.io.File.class);
+            method.invoke(revealFileAction, file);
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                Class revealFileAction = Class.forName("com.intellij.ide.actions.ShowFilePathAction");// IDEA 2019.1
+                Method method = revealFileAction.getMethod("openFile",  File.class);
+                method.invoke(revealFileAction, file);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                Messages.showErrorDialog("This action isn't supported on the current platform", "Cannot Open File");
+//                NotificationUtils.showNotification("Open file failed:" + ex.getMessage(), NotificationType.ERROR);
+            }
+        }
+    }
+
+
 
 //        @Override
 //        public void update(AnActionEvent e) {
@@ -35,6 +59,6 @@ public class LocateInFinderAction extends BaseRNConsoleAction {
 
     @NotNull
     public static String getActionName() {
-        return SystemInfo.isMac ? "Reveal Project in Finder" : "Show Project in " + com.intellij.ide.actions.RevealFileAction.getFileManagerName();
+        return SystemInfo.isMac ? "Reveal Project in Finder" : "Show Project in Exeplorer";
     }
 }
--- verifyPlugin_win.bat
+++ verifyPlugin_win.bat
@@ -1,1 +1,1 @@
-java -jar verifier-cli-1.221-all.jar -runtime-dir C:\Java\jdk1.8.0_191 check-plugin ./react-native-console.jar "C:\Program Files\JetBrains\WebStorm 2019.2" "D:\Program Files\JetBrains\WebStorm 2017.3.3" "C:\Program Files\JetBrains\WebStorm 2018.1.7"
+java -jar verifier-cli-1.221-all.jar -runtime-dir C:\Java\jdk1.8.0_191 check-plugin ./react-native-console.jar "C:\Program Files\JetBrains\WebStorm 2019.1" "C:\Program Files\JetBrains\WebStorm 2019.2" "D:\Program Files\JetBrains\WebStorm 2017.3.3" "C:\Program Files\JetBrains\WebStorm 2018.1.7"
